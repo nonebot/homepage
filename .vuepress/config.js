7f8bddf92102686fc9ec5f767ce2a17d5af2d928
Richard Chien <richardchienthebest@gmail.com>
2021-05-13 21:45:25 +0800

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    title: 'NoneBot',
+    description: '跨平台的 Python 异步聊天机器人开发框架',
+    head: [
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#ffffff' }],
+        ['meta', { name: 'application-name', content: 'NoneBot' }],
+        ['meta', { name: 'apple-mobile-web-app-title', content: 'NoneBot' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon.png` }],
+        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#5bbad5' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/icons/mstile-150x150.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#00aba9' }]
+    ],
+    theme: 'antdocs',
+    themeConfig: {
+        smoothScroll: true,
+        search: false,
+        logo: "/logo.png",
+        repo: "https://github.com/nonebot",
+        editLinks: false,
+        nav: [
+            {
+                text: '首页',
+                link: '/'
+            },
+            {
+                text: '应用案例',
+                link: '/cases.md'
+            },
+            {
+                text: '论坛',
+                link: 'https://discussions.nonebot.dev/'
+            },
+        ]
+    }
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+---
+home: true
+heroImage: /logo.png
+actionBtn:
+  text: v1.x 文档
+  link: https://docs.nonebot.dev/
+  type: primary
+  size: large
+preactionBtn:
+  text: v2.x 文档 (alpha)
+  link: https://v2.nonebot.dev/
+  type: primary
+  ghost: true
+  size: large
+features:
+- title: 简洁
+  details: 提供极其简洁易懂的 API，使你可以毫无压力地开始验证你的绝佳创意，只需编写最少量的代码，即可实现丰富的功能。
+- title: 易于扩展
+  details: 精心设计的消息处理流程使得你可以很方便地将原型扩充为具有大量实用功能的完整聊天机器人，并持续保证扩展性。
+- title: 高性能
+  details: 采用异步 I/O，利用 WebSocket 进行通信，以获得极高的性能；同时，支持使用多账号同时接入，减少业务宕机的可能。
+footer: Copyright © 2021 NoneBot Team
+footerColumn: 2
+footerWrap: 
+- headline: 生态
+  items:
+  - title: NoneBot v1
+    link: https://github.com/nonebot/nonebot
+  - title: NoneBot v2
+    link: https://github.com/nonebot/nonebot2
+  - title: OneBot 标准
+    link: https://onebot.page.moe/
+- headline: 社区
+  items:
+  - title: 论坛
+    link: https://discussions.nonebot.dev/
+  - title: "交流群 (QQ: 768887710)"
+    link: https://jq.qq.com/?_wv=1027&k=5OFifDh
+---
